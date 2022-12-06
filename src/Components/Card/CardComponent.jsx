8788cecd287fd11fc373a32f6c805e2e39c917ed
tamimhasan19702 <tamimhasancu@gmail.com>
2022-12-07 02:11:41 +0600

    added the robo images

--- src/Components/Card/CardComponent.jsx
+++ src/Components/Card/CardComponent.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './card.style.css';
 
 function CardComponent(Props) {
   return (
-    <div>
+    <div className='card-container'>
+      <img src={`https://robohash.org/${Props.monster.id}set=set1&size=180*180`} alt="monster" />
       <h1>{Props.monster.name}</h1>
     </div>
   )
